eed9ca6fbaaec5bd306deb5ca939d5a3239bbbd9
MRomHerr <122524394+MRomHerr@users.noreply.github.com>
2023-01-31 21:40:39 +0100

    Update PintaEmojis.java

--- src/pintaemojis/PintaEmojis.java
+++ src/pintaemojis/PintaEmojis.java
@@ -2,8 +2,9 @@ package pintaemojis;
 
 public class PintaEmojis {
 	public static void main(String[] args) {
-	for (int i=0; i<10; i++) {
-		System.out.printf("\uD83D\uDE00");
+		for (int i=0; i<10; i++) {
+			System.out.printf("\uD83D\uDE00");
+			System.out.println("\uD83D\uDE02");
 		}
 	}
 }
